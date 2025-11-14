f072e11b9b2925befd5a36d86d47a307a06f1e99
Alberto Noriega Arena <anoriega@soincon.es>
2025-11-14 12:27:44 +0100

    Anotación repository

--- src/main/java/com/ejemploAPI/repositories/AttributeTypeValueRepository.java
+++ src/main/java/com/ejemploAPI/repositories/AttributeTypeValueRepository.java
@@ -1,6 +1,8 @@
 package com.ejemploAPI.repositories;
 
 import com.ejemploAPI.models.AttributeTypeValue;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface AttributeTypeValueRepository extends JpaRepository<AttributeTypeValue, Long> {}
